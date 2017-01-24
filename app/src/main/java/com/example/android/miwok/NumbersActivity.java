4239d287868b8665f57d9de9fbde3b51380b147f
sonu <sonu190893@gmail.com>
2017-01-24 15:27:03 +0530

    numbers are displayed in NumberActivity by adding textView through java code

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        int index = 0;
+        int length = words.size();
+        while(index < length){
+            TextView textView = new TextView(this);
+            textView.setText(words.get(index));
+            rootView.addView(textView);
+
+            index++;
+        }
+
     }
 }
